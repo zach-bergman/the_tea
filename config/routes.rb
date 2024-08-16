a953eb4186f12bf250b1038e8f7f03ad99682baf
Zach Bergman <bzach634@gmail.com>
2024-08-16 14:26:32 -0600

    feat: Add endpoint to get all active/cancelled customer subs

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :customer_subscriptions, only: [:create, :update]
+      get '/customer_subscriptions/:customer_id', to: 'customer_subscriptions#index'
     end
   end
 end
